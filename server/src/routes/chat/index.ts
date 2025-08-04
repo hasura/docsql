9ed4724411613ef06b17adc8b1e1c6a56a620a69
Rob Dominguez <rob.dominguez@hasura.io>
2025-08-04 13:33:12 -0700

    Server: Add CORS for convo state

--- server/src/routes/chat/index.ts
+++ server/src/routes/chat/index.ts
@@ -9,6 +9,12 @@ export const chatRoutes = new Elysia({ prefix: "/chat" })
     set.headers["Access-Control-Allow-Headers"] = "Content-Type";
     return "";
   })
+  .options("/conversations/:conversationId/state", ({ set }) => {
+    set.headers["Access-Control-Allow-Origin"] = "*";
+    set.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS";
+    set.headers["Access-Control-Allow-Headers"] = "Content-Type";
+    return "";
+  })
   .get("/conversations/:conversationId/state", (context) => {
     context.set.headers["Access-Control-Allow-Origin"] = "*";
     return getConversationState(context);
